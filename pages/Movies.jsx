f866aa1d2906ceecdf253c02ec0f33eb67a005bf
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-05-12 18:17:08 +0200

    feat: install axios and add cards

--- pages/Movies.jsx
+++ pages/Movies.jsx
@@ -1,14 +1,32 @@
 import MovieCard from "../components/MovieCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function MoviesPage() {
+
+    const [movies, setMovies] = useState([])
+
+    function getMovies() {
+        axios.get('http://127.0.0.1:3000/movies')
+            .then(response => {
+                console.log(response.data);
+                setMovies(response.data);
+            })
+            .catch(err => console.log(err))
+    }
+
+    useEffect(getMovies, []);
+
     return (
         <>
             <h1>Movie</h1>
             <section>
                 <div className="row">
-                    <div className="col-12 col-md-4">
-                        <MovieCard />
-                    </div>
+                    {movies.length ? movies.map(movie =>
+                        <div className="col-12 col-md-4">
+                            <MovieCard />
+                        </div>) : <div>Movie not found</div>}
+
                 </div>
             </section>
 
