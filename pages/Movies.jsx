dc2f7197e6418df16cf7e7e5b235c1aa8605ec57
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-05-12 17:23:48 +0200

    feat: add card component

--- pages/Movies.jsx
+++ pages/Movies.jsx
@@ -1,7 +1,16 @@
+import MovieCard from "../components/MovieCard";
+
 function MoviesPage() {
     return (
         <>
-            <div>Movie</div>
+            <h1>Movie</h1>
+            <section>
+                <div className="row">
+                    <div className="col-12 col-md-4">
+                        <MovieCard />
+                    </div>
+                </div>
+            </section>
 
         </>
     )
