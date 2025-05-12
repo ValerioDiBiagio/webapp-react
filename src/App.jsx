a0c870af6993847c25c2a1112c84030319e82ac0
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-05-12 16:27:22 +0200

    feat: add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
+        <Route path="/" element={<div> Hompage</div>} />
+        <Route path="/movies" element={<div> Movies List</div>} />
+        <Route path="/movies/:id" element={<div> Movies Details</div>} />
+        <Route path="*" element={<div>404</div>} />
       </Routes>
-
-
     </BrowserRouter>
   )
 }
